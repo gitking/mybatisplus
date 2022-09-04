ca9fd3138fb3e89a3ef36a970988c6395d266955
gitking <yale268sh@163.com>
2022-09-04 17:29:40 +0800

    StopWatch秒表的使用

--- src/main/java/com/atguigu/demo/java/stopwatch/StopWatchDemo.java
+++ src/main/java/com/atguigu/demo/java/stopwatch/StopWatchDemo.java
@@ -224,7 +224,7 @@ public class StopWatchDemo {
         // watch.start();
 
         Thread.sleep(1000);
-        System.out.println("统计从开始到现在运行的时间:" + watch.getTime() + "ms,毫秒");
+        System.out.println("testApacheCommonsLang3,统计从开始到现在运行的时间:" + watch.getTime() + "ms,毫秒");
 
         Thread.sleep(1000);
         watch.split();
@@ -248,14 +248,14 @@ public class StopWatchDemo {
 
         // 上面suspend暂停后，这里想要重新统计，需要恢复一下
         watch.resume();
-        System.out.println("恢复后执行的时间是： " + watch.getTime());
+        System.out.println("恢复后执行的时间是： " + watch.getTime() + "ms,毫秒");
 
         Thread.sleep(1000);
         watch.stop();
 
         System.out.println("花费的时间》》》" + watch.getTime() + "ms,毫秒");
         // 直接转成秒s
-        System.out.println("花费的时间》》》" + watch.getTime(TimeUnit.SECONDS) + "s,秒");
+        System.out.println("testApacheCommonsLang3,花费的时间》》》" + watch.getTime(TimeUnit.SECONDS) + "s,秒");
     }
 
 
